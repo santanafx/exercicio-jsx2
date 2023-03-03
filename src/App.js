ea947de68b4d5b238d01087d481d7bf042e91401
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-03-03 10:58:22 -0300

    Primeiros comandos react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
 
-function App() {
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = mario;
+  const total = dados.compras.map((dado) => Number(dado.preco.replace("R$ ", ""))).reduce((anterior, atual) => anterior + atual);
+
+  console.log(total);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>Total: {total}</p>
+      <p>
+        Situação: <span style={{ color: dados.ativa ? "green" : "red" }}>{dados.ativa ? "Ativado" : "Desativado"}</span>{" "}
+      </p>
+      {total > 10000 && <p>Você gasto mais de R$10000</p>}
+    </>
   );
-}
+};
 
 export default App;
